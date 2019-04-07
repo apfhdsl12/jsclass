492d43b4f77b1dbf097504a7f1800c2eb2b42bdc
seongbin <apfhdsl12@naver.com>
2019-04-07 13:08:00 +0900

    ja0407

---
+++ test.js
@@ -0,0 +1,28 @@
+    
+
+
+// function workA(cb){
+//     setTimeout(function(){
+//         console.log('A 완료')
+//         cb()
+//     },3000);
+// }
+
+// function workB(cb){
+//     setTimeout(function(){
+//         console.log('B 완료')
+//         cb()
+//     },2000)
+// }
+
+// function workC(cb){
+//     setTimeout(function(){
+//         console.log('C 완료')
+//         cb()
+//     },1000)
+// }
+// //외부와의 통신, 시스템 명령어 사용할 때 (타이머)... IO작업 할 때 비동기적으로 수행한다.
+
+// workA();
+// workB();
+// workC();
